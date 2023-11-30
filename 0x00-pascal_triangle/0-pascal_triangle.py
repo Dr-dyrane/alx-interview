856b1405aa359ebd1932d311929d5a6ccd56b952
Dr-dyrane <halodyrane@gmail.com>
2023-11-30 17:05:14 +0100

    modified:   0x00-pascal_triangle/0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,7 +18,7 @@ def pascal_triangle(n):
     for i in range(n):
         row = []
         for j in range(i + 1):
-            if j==0 or j==i:
+            if j == 0 or j == i:
                 row.append(1)
             else:
                 row.append(triangle[i-1][j-1] + triangle[i-1][j])
