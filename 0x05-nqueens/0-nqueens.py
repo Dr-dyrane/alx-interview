2714808d6e9136d8d38cb4952c98057a52326008
Dr-dyrane <halodyrane@gmail.com>
2024-01-10 09:46:15 +0100

    modified:   0x05-nqueens/0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -72,46 +72,35 @@ def xout(board, row, col):
         row (int): The row where a queen was last played.
         col (int): The column where a queen was last played.
     """
-    # X out all forward spots
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
-    # X out all backwards spots
-    for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
-    # X out all spots below
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
-    # X out all spots above
-    for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
-    # X out all spots diagonally down to the right
-    c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
+    size = len(board)
+
+    # Mark all forward, backward, below, and above spots
+    for i in range(size):
+        board[row][i] = board[i][col] = "x"
+
+    # Mark all spots diagonally down to the right
+    for r, c in zip(range(row + 1, size), range(col + 1, size)):
+        if c >= size:
             break
         board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally up to the left
-    c = col - 1
-    for r in range(row - 1, -1, -1):
-        if c < 0:
-            break
-        board[r][c]
-        c -= 1
-    # X out all spots diagonally up to the right
-    c = col + 1
-    for r in range(row - 1, -1, -1):
-        if c >= len(board):
-            break
-        board[r][c] = "x"
-        c += 1
-    # X out all spots diagonally down to the left
-    c = col - 1
-    for r in range(row + 1, len(board)):
+
+    # Mark all spots diagonally up to the left
+    for r, c in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
         if c < 0:
             break
         board[r][c] = "x"
-        c -= 1
+
+    # Mark all spots diagonally up to the right
+    for r, c in zip(range(row - 1, -1, -1), range(col + 1, size)):
+        if c >= size:
+            break
+        board[r][c] = "x"
+
+    # Mark all spots diagonally down to the left
+    for r, c in zip(range(row + 1, size), range(col - 1, -1, -1)):
+        if c < 0:
+            break
+        board[r][c] = "x"
 
 
 def recursive_solve(board, row, queens, solutions):
@@ -129,6 +118,7 @@ def recursive_solve(board, row, queens, solutions):
     """
     if queens == len(board):
         solutions.append(get_solution(board))
+
         return (solutions)
 
     for c in range(len(board)):
