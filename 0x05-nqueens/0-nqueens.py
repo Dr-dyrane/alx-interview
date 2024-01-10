1c8b8622a2c63c678ba12e2397c12262b4b5aaf6
Dr-dyrane <halodyrane@gmail.com>
2024-01-10 09:40:49 +0100

    modified:   0x05-nqueens/0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -117,7 +117,7 @@ def xout(board, row, col):
 def recursive_solve(board, row, queens, solutions):
     """
     Recursively solve an N-queens puzzle.
-    
+
     Args:
         board (list): The current working chessboard.
         row (int): The current working row.
