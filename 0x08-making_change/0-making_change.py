80f759864c562e5f7ae9103786a5907fd3b0e706
Dr-dyrane <halodyrane@gmail.com>
2024-01-29 16:51:39 +0100

    modified:   0x08-making_change/0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,6 +8,26 @@ def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
+
+    Args:
+    - coins (list): A list of positive integers representing the values of available coins.
+    - total (int): The target amount for which change needs to be made.
+
+    Returns:
+    - int: The fewest number of coins needed to meet the given total.
+           If the total is 0 or less, return 0.
+           If the total cannot be met by any combination of coins, return -1.
+
+    Notes:
+    - You can assume you have an infinite number of each denomination of coin in the list.
+    - This implementation uses dynamic programming to efficiently calculate the minimum number of coins.
+
+    Example:
+    >>> makeChange([1, 2, 25], 37)
+    7
+
+    >>> makeChange([1256, 54, 48, 16, 102], 1453)
+    -1
     """
 
     if total <= 0:
